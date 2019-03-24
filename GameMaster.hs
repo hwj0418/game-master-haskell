c02ced9546636574b64c26d76037061127bc4d33
hwj0418 <ddh0418@gmail.com>
2019-03-23 20:57:20 -0400

    Update GameMaster.hs
    monad finished, still working on monadGameMaster

--- GameMaster.hs
+++ GameMaster.hs
@@ -34,48 +34,40 @@ guessingHint secret lb ub = do
 
 instance Functor FreeGameMaster where
     -- fmap :: (a -> b) -> FreeGameMaster a -> FreeGameMaster b
-    fmap f fgm = do
-        a <- fgm
-        pure (f a) 
+    -- fmap f fgm = do
+    --     a <- fgm
+    --     pure (f a) 
     -- If you are confident with your Monad instance, you can just write
-    -- fmap = liftM
+    fmap = liftM
 
 instance Applicative FreeGameMaster where
     -- pure :: a -> FreeGameMaster a
-    pure a = Pure a
+    pure = return
     -- If you are confident with your Monad instance, you can just write
     -- pure = return
 
     -- (<*>) :: FreeGameMaster (a -> b) -> FreeGameMaster a -> FreeGameMaster b
-    func <*> fgm = do
-        f <- func
-        a <- fgm 
-        pure (f a)
+    -- func <*> fgm = do
+    --     f <- func
+    --     a <- fgm 
+    --     pure (f a)
     -- If you are confident with your Monad instance, you can just write
-    -- (<*>) = ap
+    (<*>) = ap
 
 instance Monad FreeGameMaster where
     -- return :: a -> FreeGameMaster a
-
     return a = Pure a
     -- (>>=) :: FreeGameMaster a -> (a -> FreeGameMaster b) -> (FreeGameMaster b)
-    gm >>= f = do
-        a <- gm
-        case gm of
-            Pure a -> f a
-            GMAction lb ub req -> do
-                fa <- Pure req
-                f fa
-    -- Pure a >>= f = (f a)
-    -- GMAction lb ub req >>= f -- = (return req) >>= f
-    --     case req of
-    --         Surrender -> (Pure Surrender) >>= f
-    --         Guess i -> Pure (Guess i) >>= f
+    Pure a >>= f = f a
+    GMAction lb ub req >>= f = do
+        next <- GMAction lb ub (\msg -> req msg)
+        f next 
+
 instance MonadGameMaster FreeGameMaster where
     -- gmAction :: Integer -> Integer -> FreeGameMaster PlayerMsg
     gmAction lb ub = do
-        req1 <- gmAction lb ub
-        return req1
+        req <- Pure Surrender
+        return req
         
 
 
